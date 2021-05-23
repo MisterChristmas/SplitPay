549205446e4bfb624a5865d5fb26e9f0e551dccb
MisterChristmas <49103557+MisterChristmas@users.noreply.github.com>
2021-05-23 17:51:16 -0400

    Add files via upload

---
+++ WebService/Models/User.cs
@@ -0,0 +1,11 @@
+namespace WebService.Models{
+    public class WebService{
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PassWord { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
